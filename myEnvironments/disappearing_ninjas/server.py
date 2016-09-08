48ed501225e844f1ca5fc7fcd536fa9dee2153ec
Hans Ravnaas <hravnaas@hotmail.com>
2016-09-08 14:20:34 -0700

    updates

--- myEnvironments/disappearing_ninjas/server.py
+++ myEnvironments/disappearing_ninjas/server.py
@@ -8,6 +8,27 @@ def start():
 
 @app.route('/ninja')
 def showNinjas():
-	return render_template('ninja.html')
+	return render_template('image.html', imageName="ninjas.png")
+
+@app.route('/ninja/<color>')
+def coloredNinja(color):
+	colorImageMappings = {
+		"blue": "leonardo.jpg",
+		"red": "raphael.jpg",
+		"orange" : "michelangelo.jpg",
+		"purple" : "donatello.jpg"
+	}
+	
+	try:
+		imageName = colorImageMappings[color.lower()]
+	except Exception, e:
+		return render_template('image.html', imageName="notapril.jpg")
+	else:
+		pass
+	finally:
+		pass
+	 
+	return render_template('image.html', imageName=imageName)
 
 app.run(debug=True)
+
---
+++ myEnvironments/disappearing_ninjas/static/js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('div img').attr("src", $('#fileName').attr("realImg"));
+    $('div img').attr("hidden", false);
+});
