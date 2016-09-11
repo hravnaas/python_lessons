2d206779192ba7ff14aabbd1f214f536be0140f9
Hans Ravnaas <hravnaas@hotmail.com>
2016-09-10 17:56:41 -0700

    update to the wall

--- myEnvironments/the_wall/server.py
+++ myEnvironments/the_wall/server.py
@@ -14,12 +14,13 @@ mysql = MySQLConnector(app, 'thewall')
 # Default route. User is redirected here after successful login
 @app.route('/')
 def index():
-	#pw_hash = bcrypt.generate_password_hash(password)
-	#bcrypt.check_password_hash(pw_hash, test_password_1)
-	# return render_template('login.html')
-	session["userId"] = 1 # HACK
+	if "userIdXX" not in session:
+		# User is logged in and needs to register or log in
+		return render_template('login.html')
+
+	# User is already logged in. Go to the wall
 	result = getAllMessagesAndComments()
-	return render_template('wall.html', userName="Hans", userId=session["userId"], messages=result[0], comments=result[1])
+	return render_template('wall.html', userName=session["firstName"], userId=session["userId"], messages=result[0], comments=result[1])
 
 # Retrieves all messages and their comments from the database.
 def getAllMessagesAndComments():
@@ -34,16 +35,6 @@ def getAllMessagesAndComments():
 	# Return messages and comments as separate items. That way
 	# the caller can decide if it's neccessary to combine them.
 	return [messages, comments]
-
-@app.route('/friends/<userId>/edit', methods=['POST'])
-def editFriend(id):
-	return render_template('edit.html', friend=getFriend(id))
-
-# Get a specified friend from the database.
-def getFriend(id):
-	select_query = "SELECT id, first_name, last_name, occupation, created_at, updated_at FROM friends WHERE id = :id"
-	data = {'id' : id}
-	return mysql.query_db(select_query, data)
 
 @app.route('/wall/<userId>/post_message', methods=['POST'])
 def postMessage(userId):
@@ -78,32 +69,84 @@ def postCommentToDb(userId, message_id, comment):
 	mysql.query_db(insert_query, data)
 	return redirect('/')
 
-@app.route('/friends/<id>/delete', methods=['POST'])
-def deleteFriend(id):
-	removeFriend(id)
-	return redirect('/')
+@app.route('/wall/register', methods=['POST'])
+def register():
+	first_name = request.form['first_name']
+	last_name = request.form['last_name']
+	email = request.form['email']
+	password = request.form['password']
+	cpassword = request.form['cpassword']
 
-# Delete friend with a given id
-def removeFriend(id):
-	delete_query = "DELETE FROM friends WHERE id = :id"
-	data = {"id" : id}
-	mysql.query_db(delete_query, data)
+	# TODO validation
+	hasValidationErrors = False
+	if hasValidationErrors:
+		# flash 
+		pass
 
-@app.route('/friends', methods=['POST'])
-def addFriend():
-	saveFriendToDb(request.form['first_name'], request.form['last_name'], request.form['occupation'])
-	return redirect('/')
+	userId = addUserToDb(first_name, last_name, email, bcrypt.generate_password_hash(password))
+	
+	# The user was successfully registered.
+	# Automatically do a login.
+	#return redirect('/wall/login')
+	login(userId)
+	
 
-# Adds a new friedn to the database.
+# Adds a new user to the database and returns the user ID.
 # It does not check if it already exists.
-def saveFriendToDb(first_name, last_name, occupation):
-	insert_query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
+def addUserToDb(first_name, last_name, email, encrypted_password):
+	insert_query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at, password) VALUES (:first_name, :last_name, :email, NOW(), NOW(), :password)"
 	data = {
 				'first_name': first_name,
 				'last_name' : last_name,
-				'occupation' : occupation
+				'email' : email,
+				'password' : encrypted_password
 			}
 
 	return mysql.query_db(insert_query, data)
+
+@app.route('/wall/login', methods=['POST', 'GET'])
+def login(userId = -1):
+	if "userIdX" not in session:
+		if len(request.form) < 1:
+			# User is not logged in and no info was posted. Force another login.
+			return render_template('login.html')
+
+		# Log the user in by grabbing the encrypted password from the database
+		# and comparing it to the provided password after encrypting it.
+		user = getUserByEmail(request.form["email"])
+		print user[0]["password"]
+		print request.form['password']
+		try:
+			if not bcrypt.check_password_hash(user[0]["password"], request.form['password']):
+				print "DEBUG: password is incorrect"
+				flash("Incorrect user name or password")
+				return render_template('login.html')
+		except Exception, e:
+			# Handle situation when the salt is bad, etc.
+			flash("Unexpected error, please try again (" + e.message + ")")
+			return render_template('login.html')
+
+		# Credentials look good.
+		print "DEBUG: Password check passed"
+		session['userId'] = user.id
+		session['firstName'] = user.first_name
+	
+	# User is (now) considered logged in. Go to the wall.
+	result = getAllMessagesAndComments()
+	return render_template('wall.html', userName=session["firstName"], userId=session["userId"], messages=result[0], comments=result[1])
+
+# Get specified user from the database based on provided email.
+def getUserByEmail(email):
+	# TODO handle non-existent user
+	select_query = "SELECT id, first_name, password FROM users WHERE email = :email LIMIT 1"
+	data = {'email' : email}
+	return mysql.query_db(select_query, data)
+
+# Logs the user out and indirectly redirects to the login page
+@app.route('/wall/<userId>/logout', methods=['POST'])
+def logOut(userId):
+	session.pop('userId')
+	session.pop('firstName')
+	return redirect('/')
 
 app.run(debug=True)
