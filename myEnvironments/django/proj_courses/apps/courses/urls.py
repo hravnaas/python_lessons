12a0d42cb2b10b8fa2e5d5b921a58b232cdbbf97
Hans Ravnaas <hravnaas@hotmail.com>
2016-09-17 12:22:41 -0700

    updates

--- myEnvironments/django/proj_courses/apps/courses/urls.py
+++ myEnvironments/django/proj_courses/apps/courses/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^courses/destroy/(?P<courseID>\d+)$', views.removeCourse, name = 'removeCourse'),
+    url(r'^courses/destroy/(?P<courseID>\d+)/confirm$', views.CourseRemoveConfirmation, name = 'CourseRemoveConfirmation'),
     url(r'^courses/add$', views.addCourse, name = 'addCourse')
 ]
--- myEnvironments/django/proj_courses/apps/courses/views.py
+++ myEnvironments/django/proj_courses/apps/courses/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 #from . import models
 from .models import Course, Description, Comment
 
+# TODO: Add support for comments if time allows.
+# TODO: Add sexy CSS.
+
 def index(request):
     return render(request, 'courses/index.html', { "courses" : Course.objects.all() })
 
@@ -19,7 +22,13 @@ def addCourse(request):
 
     return redirect('/')
 
+def CourseRemoveConfirmation(request, courseID):
+    # Grab the course about to be deleted and prompt user for confirmation.
+    return render(request, 'courses/confirmation.html', { "course":  Course.objects.get(id = courseID) })
+
+
 def removeCourse(request, courseID):
+    # User has confirmed deletion. It's on.
     # Delete comments associated with course.
     Description.objects.filter(course_id = courseID).delete()
 
