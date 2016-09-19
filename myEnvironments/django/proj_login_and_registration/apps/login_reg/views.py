a674e5c16884e051425cd402c7465d948ff4bbc8
Hans Ravnaas <hravnaas@hotmail.com>
2016-09-19 16:21:18 -0700

    Switched login and reg to use a fat model

--- myEnvironments/django/proj_login_and_registration/apps/login_reg/views.py
+++ myEnvironments/django/proj_login_and_registration/apps/login_reg/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import User
-import inputchk
 import bcrypt
-
-# inputchk.validateAllFields()
-
-# TODO: After login, show first name, not email.
-# TODO: Support storing and validating birthday.
 
 # Default route when launching web site.
 def index(request):
@@ -17,50 +11,25 @@ def index(request):
 # Used for registering a new user.
 def register(request):
     if request.method == 'POST':
-        inputchk.validateAllFields(request)
-
-        # Redirect to login/registration page if there are validation errors.
-        if hasErrors(request):
+        result = User.objects.register(request.POST)
+        # Redirect to login/registration page again if there are validation or
+        # general registration errors.
+        if not result["validated"] or not result["registered"]:
+            for err in result["errors"]:
+                messages.add_message(request, messages.ERROR, err)
             return redirect('/')
 
-        # Input validation passed. Save user to the database.
-        User.objects.create(
-            first_name = request.POST['First Name'],
-            last_name = request.POST['Last Name'],
-            email = request.POST['Email'],
-            password = bcrypt.hashpw(request.POST['Password'].encode(), bcrypt.gensalt()),
-            birthday = request.POST['Birthday']
-        )
-
-        return render(request, 'login_reg/success.html', { "fullName" : request.POST['First Name'], "action" : "registered" })
+        # Registration succeeded.
+        return render(request, 'login_reg/success.html', { "fullName" : result["user"].first_name, "action" : "registered" })
     return redirect('/')
 
 # Logs in an existing user.
 def login(request):
     if request.method == 'POST':
-        try:
-            existingUser = User.objects.filter(email = request.POST['Email'])
-            if bcrypt.hashpw(request.POST['Password'].encode(), existingUser[0].password.encode()) != existingUser[0].password:
-                print "bad password"
-                return redirect('/')
-            print "good password"
-     	except Exception, e:
-     	      # Handle situation when the salt is bad, etc.
-     	    print "Unexpected error, please try again (" + e.message + ")"
+        result = User.objects.login(request.POST)
+        if not result["logged_in"]:
+            for err in result["errors"]:
+                messages.add_message(request, messages.ERROR, err)
             return redirect('/')
-
-        return render(request, 'login_reg/success.html', { "fullName" : existingUser[0].first_name, "action" : "logged in" })
-
-    # Add error messages if any.
+        return render(request, 'login_reg/success.html', { "fullName" : result["user"].first_name, "action" : "logged in" })
     return redirect('/')
-
-def hasErrors(request):
-    return len(messages.get_messages(request)) > 0
-
-# Get specified user from the database based on provided email.
-def getUserByEmail(email):
-	# Note that caller needs to handle non-existent user.
-
-	select_query = "SELECT id, first_name, password FROM users WHERE email = :email LIMIT 1"
-	data = {'email' : email}
-	return mysql.query_db(select_query, data)
